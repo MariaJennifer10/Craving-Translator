a8c38ff9890000740eafa06a4c478a87f66e7f5a
Maria Jennifer <mariyajenifer.m@gmail.com>
2025-05-12 19:49:06 +0530

    Updates

---
+++ precache-manifest.a4d938de46a6bbbdd3336efc11aa4fc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7f4a0e5580acf97c22a0295f0e6d9a",
+    "url": "/Craving-Translator/index.html"
+  },
+  {
+    "revision": "22c2bf9b15299c6b456a",
+    "url": "/Craving-Translator/static/css/main.d8a321be.chunk.css"
+  },
+  {
+    "revision": "39cee13a379d8eaf15d6",
+    "url": "/Craving-Translator/static/js/2.6949fafe.chunk.js"
+  },
+  {
+    "revision": "4cb447811eafcde62d61d67183d8b566",
+    "url": "/Craving-Translator/static/js/2.6949fafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c2bf9b15299c6b456a",
+    "url": "/Craving-Translator/static/js/main.48eaa223.chunk.js"
+  },
+  {
+    "revision": "57bdbb2f4b544aae8e9d",
+    "url": "/Craving-Translator/static/js/runtime-main.23f3951c.js"
+  }
+]);
